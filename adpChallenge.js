8076e38b632aecff1bc7118849051b5b3385e190
jeromeaking40 <jeromeaking40@yahoo.com>
2018-11-13 17:22:11 -0500

    first commit

---
+++ adpChallenge.js
@@ -0,0 +1,21 @@
+//Create a function that takes an array of numbers and returns an array of only the odd numbers.
+// The function must not use looping or libraries that use looping.
+
+// Array of numbers
+let numbers = [2, 3, 4, 5, 6, 17, 28, 39, 41, 52, 54, 75, 83, 109, 58, 77, 93];
+
+// Find out if a number is odd
+function isOddNumber(number) {
+	if (number % 2) {
+		return number;
+	}
+}
+
+// Push all odd numbers into new array
+function getOddNumbers() {
+	let oddNumbers = numbers.filter(isOddNumber); //Is array.filter considered an loop?
+	console.log(oddNumbers);
+}
+
+// Call the function
+getOddNumbers(numbers);
