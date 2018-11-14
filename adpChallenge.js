91507ee02da7932503aa9290ee11bb21d11844db
jeromeaking40 <jeromeaking40@yahoo.com>
2018-11-13 19:05:33 -0500

    Added console logs for clarity

--- adpChallenge.js
+++ adpChallenge.js
@@ -7,6 +7,10 @@ let numbers = [2, 3, 4, 5, 6, 17, 28, 39, 41, 52, 54, 75, 83, 109, 58, 77, 93];
 // Find out if a number is odd
 function isOddNumber(number) {
 	if (number % 2) {
+		console.log('Number is odd:', number);
+		return number;
+	} else {
+		console.log('Number is even:', number);
 		return number;
 	}
 }
