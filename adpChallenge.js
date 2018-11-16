ba01604e43e8cbb97da8e683ee43a8b9737148a4
jeromeaking40 <jeromeaking40@yahoo.com>
2018-11-16 12:40:04 -0500

    Refactored

--- adpChallenge.js
+++ adpChallenge.js
@@ -2,22 +2,45 @@
 // The function must not use looping or libraries that use looping.
 
 // Array of numbers
-let numbers = [2, 3, 4, 5, 6, 17, 28, 39, 41, 52, 54, 75, 83, 109, 58, 77, 93];
+// Each number needs to be tested against the isOddNumber function
+let numbers = [
+	2,
+	3,
+	4,
+	5,
+	6,
+	17,
+	28,
+	39,
+	41,
+	52,
+	54,
+	75,
+	83,
+	109,
+	58,
+	77,
+	93,
+	133,
+	230,
+	305
+];
 
-// Find out if a number is odd
-function isOddNumber(number) {
-	if (number % 2) {
-		console.log('Number is odd:', number);
-		return number;
-	} else {
-		console.log('Number is even:', number);
-		return number;
+// Push all odd numbers into this array;
+let oddNumbers = [];
+
+// Check to see if a number is odd
+function isOddNumber(c, d) {
+	if (c % 2 & d % 2) {
+		oddNumbers.push(c);
+		return oddNumbers;
 	}
 }
 
-// Push all odd numbers into new array
-function getOddNumbers() {
-	let oddNumbers = numbers.filter(isOddNumber); //Is array.filter considered an loop?
+// Return array of only odd numbers in this function.
+function getOddNumbers(nums = []) {
+	// Is sorting an array considered looping? I don't think so. Might be wrong?!?!?!
+	nums.sort(isOddNumber);
 	console.log(oddNumbers);
 }
 
